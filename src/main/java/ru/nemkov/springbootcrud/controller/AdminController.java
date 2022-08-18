ddcef4baec5bcfd841382c3206d68ab5df548084
NemSnK <nemsnknet@yandex.ru>
2022-08-18 18:38:54 +0300

    without edit

--- src/main/java/ru/nemkov/springbootcrud/controller/AdminController.java
+++ src/main/java/ru/nemkov/springbootcrud/controller/AdminController.java
@@ -20,16 +20,18 @@ public class AdminController {
     private final RoleService roleService;
     private final UserService userService;
 
+
     @Autowired
     public AdminController(RoleService roleService, UserService userService) {
         this.roleService = roleService;
         this.userService = userService;
     }
-
+    //Главная страница
     @GetMapping()
     public String showAllUsers(Principal principal, Model model, @RequestParam(defaultValue = "1") int page) {
         model.addAttribute("user", userService.findByUsername(principal.getName()));
         model.addAttribute("page", page);
+        model.addAttribute("deleted");
         if (page == 2) {
             model.addAttribute("newuser", new User());
         } else {
@@ -37,20 +39,7 @@ public class AdminController {
         }
         return "admin/index";
     }
-
-
- /*  @GetMapping("/{id}")
-    public String showUser(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "user/show";
-    }*/
-
-  /*  @GetMapping("/new")
-    public String showNewUserForm(Model model) {
-        model.addAttribute("user", new User());
-        return "admin?page=2";
-    }*/
-
+    //Добавление нового пользователя
     @PostMapping()
     public String createUser(Principal principal, Model model, @ModelAttribute("newuser") @Valid User user,
                              BindingResult bindingResult, Long newRole) {
@@ -71,24 +60,29 @@ public class AdminController {
         userService.addUser(user);
         return "redirect:/admin";
     }
-
-    @GetMapping("/{id}/edit")
-    public String showEditUserForm(Model model, @PathVariable("id") Long id) {
-        model.addAttribute("user", (User) userService.getUserById(id));
-        return "admin/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String updateUser(@ModelAttribute("user") @Valid User user,
-                             BindingResult bindingResult, @PathVariable("id") Long id) {
-        if (bindingResult.hasErrors()) return "/edit";
-        userService.updateUser(id, user);
-        return "redirect:/admin";
-    }
-
+    //Удаление пользователя
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.removeUser(id);
         return "redirect:/admin";
     }
+
+    /*    @GetMapping("/{id}/delete")
+    public String showDeleteUserForm(Principal principal, Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        model.addAttribute("page", 1);
+        model.addAttribute("deleted", (User)userService.getUserById(id));
+        delFormShow = true;
+        return "admin#delete";
+    }*/
+
+ /*   @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") @Valid User user,
+                             BindingResult bindingResult, @PathVariable("id") Long id) {
+        if (bindingResult.hasErrors()) return "/edit";
+        userService.updateUser(id, user);
+        return "redirect:/admin";
+    }*/
+
+
 }
